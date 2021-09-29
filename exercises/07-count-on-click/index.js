b6f4ff82254e76ccf0050ee2dfa9c039aa435636
AlexisAlegria <alexisalegriav@gmail.com>
2021-09-29 23:17:47 +0000

    Finished

--- exercises/07-count-on-click/index.js
+++ exercises/07-count-on-click/index.js
@@ -1,10 +1,12 @@
 //This is a global variable
 var counter = 0;
+var counterDecrease = 100;
 
 window.onload = function loadFunction()
 {
 	//here I set the screen to the initial value when the website is fully loaded.
 	document.getElementById('screen').innerHTML = "The counter value is "+counter;
+	document.getElementById('screendecrease').innerHTML = "The counter value is "+counterDecrease;
 }
 
 //called when the user clicks
@@ -14,4 +16,10 @@ window.increaseCounter = function increaseCounter()
 	counter++;
 	//update the screen with the new value
 	document.getElementById('screen').innerHTML = "The counter value is "+counter;
+}
+
+window.decreaseCounter = function decreaseCounter()
+{
+	counterDecrease--;
+	document.getElementById('screendecrease').innerHTML = "The counter value is "+counterDecrease;
 }
